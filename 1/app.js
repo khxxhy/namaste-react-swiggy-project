18545c2d79a79f2cf2656ff5de71a6f6e59021cf
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-06-21 19:59:04 +0530

    css changes

--- 1/app.js
+++ 1/app.js
@@ -19,10 +19,34 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return ( 
+  <div className="res-card">
+    <img className="res-logo" 
+      alt="res-logo"
+      src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ajwonvkychxzkbvyq8dv">
+    </img>
+    <h3>Kishori</h3>
+  </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard/>
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
